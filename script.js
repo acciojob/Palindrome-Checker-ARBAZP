98ef250e2dab6f2dc7a8cdae47954b8a3b2a63d1
ARBAZP <117530037+ARBAZP@users.noreply.github.com>
2023-01-22 00:26:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
+let string = str.toLowerCase()
 
+let reverse = ''
+for(let i= string.length-1; i>=0; --i){
+    reverse += string.charAt(i)
+}
+if (string === reverse) return true
+
+return false
 }
 module.exports = palindrome
